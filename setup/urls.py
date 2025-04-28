cfee873dd6efa391685d0c1021d700f61bc10621
Balarajjj <joaovictorsmatheus@gmail.com>
2025-04-27 22:49:38 -0300

    feat: pasta .env configurada, primeira migration e db, settings com black

--- setup/urls.py
+++ setup/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
@@ -24,9 +25,6 @@ Nessa parte aqui eu to importando dos meus próprios arquivos de views
 
 """
 
-from todos.views import home 
+from todos.views import home
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', home)
-]
+urlpatterns = [path("admin/", admin.site.urls), path("", home)]
